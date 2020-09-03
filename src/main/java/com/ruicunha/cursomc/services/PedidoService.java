1bc17c696633c37b5ecf3f326a56cbbac344ffdf
Rui Cunha <rui_filipe-5@hotmail.com>
2020-09-03 11:51:30 +0100

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/ruicunha/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.ruicunha.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ruicunha.cursomc.domain.Pedido;
+import com.ruicunha.cursomc.repositories.PedidoRepository;
+import com.ruicunha.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
