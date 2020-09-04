260101aa4ef2703a92c4f31ae457dbc2beaf9417
Rui Cunha <rui_filipe-5@hotmail.com>
2020-09-04 20:08:31 +0100

    Subtotal e total do Pedido

--- src/main/java/com/ruicunha/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/ruicunha/cursomc/repositories/ProdutoRepository.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ruicunha.cursomc.domain.Categoria;
 import com.ruicunha.cursomc.domain.Produto;
@@ -14,6 +16,8 @@ import com.ruicunha.cursomc.domain.Produto;
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 
+	@Transactional(readOnly=true)
 	//@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
-	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
+	//Na forma usada não precisa de conter os @Param("...")
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
 }
