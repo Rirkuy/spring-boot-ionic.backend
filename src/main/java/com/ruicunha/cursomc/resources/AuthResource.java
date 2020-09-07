161dc3d5ed432d363f45fd641fc9a69ca35f75d9
Rui Cunha <rui_filipe-5@hotmail.com>
2020-09-07 22:05:36 +0100

    Refresh token

---
+++ src/main/java/com/ruicunha/cursomc/resources/AuthResource.java
@@ -0,0 +1,24 @@
+package com.ruicunha.cursomc.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ruicunha.cursomc.security.UserSS;
+import com.ruicunha.cursomc.services.UserService;
+
+@RestController
+@RequestMapping(value = "/path")
+public class AuthResource {
+
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+}
--- src/main/java/com/ruicunha/cursomc/services/UserService.java
+++ src/main/java/com/ruicunha/cursomc/services/UserService.java
@@ -6,7 +6,7 @@ import com.ruicunha.cursomc.security.UserSS;
 
 public class UserService {
 
-	public static UserSS autheticated() {
+	public static UserSS authenticated() {
 		try {
 			return (UserSS) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		}
