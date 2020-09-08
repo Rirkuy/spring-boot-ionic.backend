508e76c8171df32ea6852e240660242230f4a7d5
Rui Cunha <rui_filipe-5@hotmail.com>
2020-09-08 12:44:34 +0100

    Salvando a URL da imagem em Cliente

--- src/main/java/com/ruicunha/cursomc/services/S3Service.java
+++ src/main/java/com/ruicunha/cursomc/services/S3Service.java
@@ -1,6 +1,5 @@
 package com.ruicunha.cursomc.services;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
@@ -13,11 +12,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.ruicunha.cursomc.services.exceptions.FileException;
 
 @Service
 public class S3Service {
@@ -37,7 +34,7 @@ public class S3Service {
 			String contentType = multipartFile.getContentType();
 			return uploadFile(is, fileName, contentType);
 		} catch (IOException e) {
-			throw new RuntimeException("Erro de IO: " + e.getMessage());
+			throw new FileException("Erro de IO: " + e.getMessage());
 		}
 		
 	}
@@ -51,7 +48,7 @@ public class S3Service {
 			LOG.info("Upload concluído");
 			return s3client.getUrl(bucketName, fileName).toURI();
 		} catch (URISyntaxException e) {
-			throw new RuntimeException("Erro ao converter URL para URI");
+			throw new FileException("Erro ao converter URL para URI");
 		}
 	}
 
